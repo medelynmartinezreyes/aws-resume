a5c27a04c3924a5692becd9ccf64f88b4180b3cd
MEDELYNM <medelynmartinezreyes@gmail.com>
2024-11-07 12:46:51 -0500

    adding lambda url

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("Your-LambdaFunction-URL");
+    let response = await fetch("https://fkqp6mxbviqy2jem3ghhe23mve0ypxxw.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
