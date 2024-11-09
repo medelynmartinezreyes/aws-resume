079a5f8c60f6e1d51fb12cb13b52e04c4e7e5b39
MEDELYNM <medelynmartinezreyes@gmail.com>
2024-11-08 20:09:02 -0500

    updated site with resume info and added a waving emoji

--- website/js/index.js
+++ website/js/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://fkqp6mxbviqy2jem3ghhe23mve0ypxxw.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
+    counter.innerHTML = ` This page has ${data} views!`;
 }
 
 updateCounter();
